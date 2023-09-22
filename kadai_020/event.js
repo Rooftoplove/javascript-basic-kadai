44717e7c5e16bec64c7f2f25d37f3695e9b7eeda
屋上 <143565301+Rooftoplove@users.noreply.github.com>
2023-09-22 18:04:01 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+// textというidを持つHTML要素を取得し、定数に代入する
+const oneclick = document.getElementById('text');
+ 
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', function() {
+  setTimeout(function() {
+    oneclick.textContent='ボタンをクリックしました';
+  }, 2000);
+});
