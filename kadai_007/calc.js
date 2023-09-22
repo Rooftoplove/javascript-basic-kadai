5a27fe04042010ba7824106602b21073677e0293
屋上 <143565301+Rooftoplove@users.noreply.github.com>
2023-09-22 11:58:40 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,4 +1,4 @@
-num = 60
+num = 7
  
 // 変数numの値を出力する（確認用）
 console.log(num);
@@ -14,4 +14,8 @@ else if ((num % 5) == 0) {
 // 変数numの値が3の倍数であれば、「3の倍数です」という文字列を出力する
 else if ((num % 3) == 0) {
   console.log('3の倍数です');
+}
+// 上記以外の場合、値を表示
+else{
+  console.log(num);
 }
